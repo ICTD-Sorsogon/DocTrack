c99ec995d01eeec2e6a8235dc68c4d8ee85fb47c
JPGrefaldo <johnpaulgrefaldo@gmail.com>
2021-01-13 16:54:29 +0800

    add admin role

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -19,6 +19,7 @@ class UsersTableSeeder extends Seeder
     {
         User::factory()->create(['username' => 'GO', 'role_id' => 2, 'office_id' => '1']);
         User::factory()->create(['username' => 'VCO', 'role_id' => 2, 'office_id' => '2']);
+        User::factory()->create(['username' => 'admin', 'role_id' => 1, 'office_id' => '1']);
         User::factory()->times(2)->create(['role_id'=>1]);
         User::factory()->times(10)->create(['role_id'=>2]);
         User::factory()->times(2)->create(['role_id'=>3]);
