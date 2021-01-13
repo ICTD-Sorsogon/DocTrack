392ba965b241a8b114aa408e7be52fab99879f6a
JPGrefaldo <johnpaulgrefaldo@gmail.com>
2021-01-13 16:55:14 +0800

    remove roles on components

--- resources/js/components/HomeContainer.vue
+++ resources/js/components/HomeContainer.vue
@@ -1,10 +1,7 @@
 <template>
     <div>
         <div v-if="auth_user">
-            <div v-if="auth_user.role_id === 1">
-                <!-- <admin-dashboard-component :user="user"></admin-dashboard-component> -->
-            </div>
-            <div v-if="auth_user.role_id === 2">
+            <div>
                 <user-home-component></user-home-component>
             </div>
         </div>
