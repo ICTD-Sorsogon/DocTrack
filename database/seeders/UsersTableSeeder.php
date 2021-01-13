012f3e367a7f417e7037703252ff1b477e7d6565
JPGrefaldo <johnpaulgrefaldo@gmail.com>
2021-01-13 08:53:53 +0800

    update user Seeder

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -17,7 +17,8 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        User::factory()->create(['username' => 'admin', 'role_id' => 2]);
+        User::factory()->create(['username' => 'GO', 'role_id' => 2, 'office_id' => '1']);
+        User::factory()->create(['username' => 'VCO', 'role_id' => 2, 'office_id' => '2']);
         User::factory()->times(2)->create(['role_id'=>1]);
         User::factory()->times(10)->create(['role_id'=>2]);
         User::factory()->times(2)->create(['role_id'=>3]);
