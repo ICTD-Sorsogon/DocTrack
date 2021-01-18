3b51dd706e3cbbcf7550581de02260b4c8d16fb2
kennael22 <johnkevin.noguera@gmail.com>
2021-01-18 11:19:46 +0800

    add table traccking records

--- resources/js/components/user/ReportAging.vue
+++ resources/js/components/user/ReportAging.vue
@@ -4,14 +4,18 @@
             Tracking Reports
         </v-card-title>
         <v-row>
-            <!-- <v-col cols="12">
+            <v-col cols="12">
                   <v-data-table
                     :headers="headers"
                     :items="all_documents"
                     :items-per-page="5"
                     class="elevation-1"
-                ></v-data-table>
-            </v-col> -->
+                >
+                <template v-slot:[`item.effiency`] = "{ item }">
+                    {{item.effiency}}%
+                </template>
+                </v-data-table>
+            </v-col>
         </v-row>
     </v-card>
 </template>
@@ -28,8 +32,8 @@ export default {
                     value: 'name',
                 },
                 { text: 'All Transaction', value: 'trackingrecords.length' },
-                { text: 'Delayed Document Code', value: '' },
-                { text: 'Efficiency Rating', value: 'carbs' },
+                { text: 'Delayed Document Code', value: 'delayed' },
+                { text: 'Efficiency Rating', value: 'effiency' },
             ],
         }
     },
@@ -37,17 +41,26 @@ export default {
         ...mapGetters(['getDocument']),
         all_documents(){
             let alldocuments = this.$store.state.documents.allDocuments;
-            alldocuments.array.forEach(element => {
+            alldocuments.forEach(element => {
                 element.delayed = 0;
+                element.effiency = 0;
+                element.tracklength = 0;
                 element.trackingrecords.forEach(el => {
-                    console.log(el);
+                    var last_touched = new Date(el.last_touched);
+                    var now = new Date(el.created_at);
+                    var delay = now.getDate() - last_touched.getDate();
+                    if (delay > 7) element.delayed +=1;
+                    element.tracklength = element.trackingrecords.length;
+                    element.effiency = ((element.tracklength - element.delayed)
+                    / element.tracklength) * 100;
+
                 })
             });
             return alldocuments;
         }
     },
     mounted() {
-        this.all_documents;
+        console.log(this.all_documents);
         this.$store.dispatch('getDocument');
         this.$store.dispatch('unsetLoader');
     }
