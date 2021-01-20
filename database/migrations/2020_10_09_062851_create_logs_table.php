7076881c4b793d7c197c518de12562e9615c8afd
Kenneth Solomon <synecders@gmail.com>
2021-01-20 16:16:30 +0800

    change remarks to longtext

--- database/migrations/2020_10_09_062851_create_logs_table.php
+++ database/migrations/2020_10_09_062851_create_logs_table.php
@@ -17,7 +17,7 @@ class CreateLogsTable extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained('users');
             $table->string('action', 50);
-            $table->string('remarks', 50);
+            $table->longText('remarks');
             // $table->unsignedBigInteger('item_id')->nullable();
             // $table->longText('original_values')->nullable();
             // $table->longText('new_values')->nullable();
