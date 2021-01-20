e2edf4dcf6071eb182ef584ed2c8b7202b1ca86c
Kenneth Solomon <synecders@gmail.com>
2021-01-20 15:25:43 +0800

    Event and Listener

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Events\NewDocumentHasAddedEvent;
+use App\Listeners\InsertDocumentListener;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -15,9 +17,12 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
+        NewDocumentHasAddedEvent::class => [
+            InsertDocumentListener::class,
         ],
+        // Registered::class => [
+        //     SendEmailVerificationNotification::class,
+        // ],
     ];
 
     /**
