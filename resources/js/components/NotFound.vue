36443737f503231652f8e1484409b298ab8263eb
Shannon Retuerma <snretuerma@up.edu.ph>
2021-01-22 10:44:14 +0800

    Initial not found page

--- resources/js/components/NotFound.vue
+++ resources/js/components/NotFound.vue
@@ -1,11 +1,46 @@
 <template>
-<v-container fill-height>
-    <v-row justify="center" align="center">
-        <v-col cols="12" sm="4">
-            404 Content Not Found
-        </v-col>
-    </v-row>
-</v-container>
+<v-app>
+    <v-main>
+        <v-container fill-height fluid>
+        <v-row
+            align="center"
+            justify="center"
+            class="text-center"
+        >
+            <v-col cols="12">
+                <v-icon
+                    size="25em"
+                    color="black"
+                >
+                    mdi-shredder
+                </v-icon>
+            </v-col>
+            <v-col cols="12">
+                 <h1>
+                    PAGE NOT FOUND
+                </h1>
+            </v-col>
+            <v-col cols="12">
+                <v-btn
+                    outlined
+                    color="primary"
+                    link
+                    to="/"
+                >
+                    <v-icon
+                        left
+                        dark
+                    >
+                        mdi-home
+                    </v-icon>
+                    Back to Home
+                </v-btn>
+            </v-col>
+        </v-row>
+        </v-container>
+    </v-main>
+</v-app>
+
 </template>
 
 <script>
