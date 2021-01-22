b14f46c7473e818f4d36d9a9040181c422ceb399
Shannon Retuerma <snretuerma@up.edu.ph>
2021-01-22 11:46:30 +0800

    Image react to viewport size

--- resources/js/components/NotFound.vue
+++ resources/js/components/NotFound.vue
@@ -7,13 +7,27 @@
             justify="center"
             class="text-center"
         >
-            <v-col cols="12">
-                <v-icon
-                    size="25em"
-                    color="black"
+            <v-col
+                cols="12"
+                align="center"
+                justify="center"
+            >
+                <v-img
+                    src="/images/provincial_logo.png"
+                    lazy-src="/images/provincial_logo.png"
+                    :max-width="max_width"
+                    :max-height="max_height"
                 >
-                    mdi-shredder
-                </v-icon>
+                    <template v-slot:placeholder>
+                        <v-row
+                            class="fill-height ma-0"
+                            align="center"
+                            justify="center"
+                        >
+                            <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                        </v-row>
+                    </template>
+                </v-img>
             </v-col>
             <v-col cols="12">
                  <h1>
@@ -45,6 +59,25 @@
 
 <script>
 export default {
-
+    computed: {
+        max_width() {
+            switch (this.$vuetify.breakpoint.name) {
+            case 'xs': return 300
+            case 'sm': return 400
+            case 'md': return 400
+            case 'lg': return 415
+            case 'xl': return 680
+            }
+        },
+        max_height() {
+            switch (this.$vuetify.breakpoint.name) {
+            case 'xs': return 350
+            case 'sm': return 420
+            case 'md': return 450
+            case 'lg': return 455
+            case 'xl': return 720
+            }
+        }
+    },
 }
 </script>
