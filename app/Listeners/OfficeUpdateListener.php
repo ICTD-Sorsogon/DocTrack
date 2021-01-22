34eccf2ab8e805f32171ad9c9c971eece81bc413
Kenneth Solomon <synecders@gmail.com>
2021-01-22 10:58:58 +0800

    update remarks

--- app/Listeners/OfficeUpdateListener.php
+++ app/Listeners/OfficeUpdateListener.php
@@ -26,17 +26,17 @@ class OfficeUpdateListener
      */
     public function handle($event)
     {
-        $office_name = $event->request_obj->name;
-        $office_code = $event->request_obj->office_code;
         $old_values = json_encode($event->request_obj);
         $data = json_encode($event->old_values);
+        $name = json_decode($data)->name;
+        $code = json_decode($data)->office_code;
 
         $log = new Log;
         $log->user_id = $event->user_id;
         $log->new_values = $data;
         $log->original_values = $old_values;
         $log->action = 'Office Updated';
-        $log->remarks = 'Office has been Updated with new Office Name of : '.$office_name.' and Office Code of: '.$office_code;
+        $log->remarks = 'Office has been Updated with new Office Name of : '.$name.' and Office Code of: '.$code;
         return $log->save();
     }
 }
