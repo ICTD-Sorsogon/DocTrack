7dc397f8a2fb8fc78e4e726f9e6abb0997f97e14
snretuerma <snretuerma@up.edu.ph>
2021-02-23 09:59:15 +0800

    Refactor some of the code and fix an issue where some transactions are not read

--- resources/js/components/user/ReportAging.vue
+++ resources/js/components/user/ReportAging.vue
@@ -67,16 +67,16 @@ export default {
                         let acknowledged = value.filter(value => value.action === 'acknowledged');
                         if (acknowledged.length > 0) {
                             offices[acknowledged[0].office_id].transactions+= acknowledged[0].document.destination.length;
-                            offices[acknowledged[0].office_id].transaction_speed.push( new Date(acknowledged[0].created_at).getTime() - new Date(created[0].created_at).getTime() )
+                            offices[acknowledged[0].office_id].transaction_speed.push(new Date(acknowledged[0].created_at).getTime() - new Date(created[0].created_at).getTime())
                             if (new Date(acknowledged[0].created_at).getTime() -
-                                    new Date(created[0].created_at).getTime() > week_in_milliseconds) offices[value[i].office_id].delayed++
+                                    new Date(created[0].created_at).getTime() > week_in_milliseconds) offices[value[i].office_id].delayed+=acknowledged[0].document.destination.length
                             let received = value.filter(value => value.action === 'received');
                             if (received.length > 0) {
                                 received.forEach(element => {
                                     offices[element.office_id].transactions++
-                                    offices[element.office_id].transaction_speed.push( new Date(element.created_at).getTime() - new Date(acknowledged[0].created_at).getTime())
+                                    offices[element.office_id].transaction_speed.push(new Date(element.created_at).getTime() - new Date(acknowledged[0].created_at).getTime())
                                     if (new Date(element.created_at).getTime() -
-                                        new Date(acknowledged[0].created_at).getTime() > week_in_milliseconds) offices[value[i].office_id].delayed++
+                                        new Date(acknowledged[0].created_at).getTime() > week_in_milliseconds) offices[acknowledged[0].office_id].delayed++
                                 });
                             }
                         }
@@ -85,7 +85,7 @@ export default {
                             if (i !== value.length - 1) {
                                 if (new Date(value[i + 1].created_at).getTime() -
                                     new Date(value[i].created_at).getTime() > week_in_milliseconds) offices[value[i].office_id].delayed++
-                                offices[value[i].office_id].transaction_speed.push( new Date(value[i + 1].created_at).getTime() - new Date(value[i].created_at).getTime())
+                                offices[value[i].office_id].transaction_speed.push(new Date(value[i + 1].created_at).getTime() - new Date(value[i].created_at).getTime())
                             }
                             offices[value[i].office_id].transactions++;
                         }
@@ -95,24 +95,16 @@ export default {
             for (const [key, value] of Object.entries(offices)) {
                 if(value.transaction_speed.length > 0){
                     let transaction_speed_list = value.transaction_speed
-                    let transaction_min = Math.min.apply(Math, transaction_speed_list)
-                    let transaction_max = Math.max.apply(Math, transaction_speed_list)
-
-                    value.min = this.timeConversion(transaction_min)
-                    value.max = this.timeConversion(transaction_max)
+                    value.min = this.timeConversion(Math.min.apply(Math, transaction_speed_list))
+                    value.max = this.timeConversion(Math.max.apply(Math, transaction_speed_list))
                     value.mean = this.timeConversion(mean( transaction_speed_list ))
-
                 } else {
-                    value.min = 'None'
-                    value.max = 'None'
-                    value.mean = 'None'
+                    value.min = value.max = value.mean = 'Not available'
                 }
-
                 value.efficiency = value.transactions !== 0 ?`${((value.transactions - value.delayed) /
-                    value.transactions * 100).toFixed(2)}%` : 'None';
+                    value.transactions * 100).toFixed(2)}%` : 'Not available';
                 data.push(value);
             }
-
             return data;
         },
     },
@@ -123,24 +115,14 @@ export default {
     },
     methods: {
         timeConversion(millisec) {
-
-            var seconds = (millisec / 1000).toFixed(1);
-
-            var minutes = (millisec / (1000 * 60)).toFixed(1);
-
-            var hours = (millisec / (1000 * 60 * 60)).toFixed(1);
-
-            var days = (millisec / (1000 * 60 * 60 * 24)).toFixed(1);
-
-            if (seconds < 60) {
-                return seconds + " Sec";
-            } else if (minutes < 60) {
-                return minutes + " Min";
-            } else if (hours < 24) {
-                return hours + " Hrs";
-            } else {
-                return days + " Days"
-            }
+            var seconds = Math.trunc(millisec / 1000);
+            var minutes = Math.trunc(millisec / (1000 * 60));
+            var hours = Math.trunc(millisec / (1000 * 60 * 60));
+            var days = Math.trunc(millisec / (1000 * 60 * 60 * 24));
+            if (seconds < 60) return `${seconds}s`;
+            else if (minutes < 60) return `${minutes}m`;
+            else if (hours < 24) return `${hours}h`;
+            else return `${days}d`
         }
     }
 }
